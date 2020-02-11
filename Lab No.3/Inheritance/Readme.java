3d3a7ffc645a0bdcc7a2c59ba5a7466044c02435
DeepakGtm123 <53470604+DeepakGtm123@users.noreply.github.com>
2020-02-11 14:47:55 +0545

    Create Readme.java

---
+++ Lab No.3/Inheritance/Readme.java
@@ -0,0 +1,14 @@
+### Inheritance
+Inheritance is a mechanism in which one class acquires the property of another class.
+For example, a child inherits the traits of his/her parents. With inheritance, we can reuse the fields and methods of the existing class.
+Types of inheritance in java
+On the basis of class, there can be three types of inheritance in java : single, multilevel and hierarchical.
+
+* Single Inheritance
+When a class inherits another class, it is known as a single inheritance. In the example given below, Dog class inherits the Animal class, so there is the single inheritance.
+
+* Multilevel Inheritance Example
+When there is a chain of inheritance, it is known as multilevel inheritance. As you can see in the example given below, BabyDog class inherits the Dog class which again inherits the Animal class, so there is a multilevel inheritance.
+
+* Hierarchical Inheritance Example
+When two or more classes inherits a single class, it is known as hierarchical inheritance. In the example given below, Dog and Cat classes inherits the Animal class, so there is hierarchical inheritance.
