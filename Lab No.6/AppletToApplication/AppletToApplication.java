4e7bbcb22c616e33a02228180e1580251046cdfe
DeepakGtm123 <53470604+DeepakGtm123@users.noreply.github.com>
2020-02-18 09:26:53 +0545

    Update AppletToApplication.java

--- Lab No.6/AppletToApplication/AppletToApplication.java
+++ Lab No.6/AppletToApplication/AppletToApplication.java
@@ -1,1 +1,13 @@
-
+import java.awt.*;
+public class AppletToApplication{
+	AppletToApplication(){
+		int a = 5;
+	}
+	public void paint(){
+		System.out.println("Hello");
+	}
+	public static void main(String[] args){
+		AppletToApplication obj = new AppletToApplication();
+		obj.paint();
+	}
+}
